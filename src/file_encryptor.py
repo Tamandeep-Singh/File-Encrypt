6256bc80dbee9a88b0881a5addc4aa68fc450b43
Tamandeep Singh <tamandeep.singh.business@gmail.com>
2022-12-28 17:31:15 +0000

    added a method to save the encrypted file data to a JSON file with additional metadata (timestamp and file processed) and the ability to save the random key generated in its own file

--- src/file_encryptor.py
+++ src/file_encryptor.py
@@ -1,9 +1,13 @@
 import file_utils as FileUtils
+import time
 from encryptor import Encryptor
 from logger import Logger
 
 
 file_encryptor_logger = Logger("[FileEncryptor]")
+
+def get_epoch_timestamp():
+    return int(time.time())
 
 
 def get_encryptor_instance():
@@ -12,8 +16,21 @@ def get_encryptor_instance():
     if key is None:
         key = Encryptor.generate_random_key()
         file_encryptor_logger.log(f"<get_crypto_key>: Key file not found. Created Random Key: {key}")
+        FileUtils.write_to_file("../cryptography_key.txt", key.decode('utf-8'), "w")
     
     return Encryptor(key)
+
+
+def save_to_json_file(filename, data):
+    original_file = FileUtils.get_file_without_ext(filename)
+    encryption_filename = original_file + "_encrypted.json"
+    data = { 
+        'processed_file': filename,
+        'data': data,
+        'timestamp': get_epoch_timestamp()
+    }
+    FileUtils.write_to_file_json(encryption_filename, data, 'w')
+
 
         
 
@@ -23,8 +40,10 @@ def process_file_data(filename):
         return
     
     fernet_encryptor = get_encryptor_instance()
-    encrypted_file_data = fernet_encryptor.encrypt(file_data)
+    encrypted_file_data = fernet_encryptor.encrypt(file_data).decode('utf-8')
     file_encryptor_logger.log(f"<process_file_data>: Encrypted File Data -> Result: {encrypted_file_data}")
+    save_to_json_file(filename, encrypted_file_data)
+
     
 
 
