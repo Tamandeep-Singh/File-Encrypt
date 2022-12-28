e1ec7312c13160b61784ffc95588585e84203959
Tamandeep Singh <tamandeep.singh.business@gmail.com>
2022-12-28 17:26:23 +0000

    added methods to write to any file (including JSON) and to get the filename without its extension

--- src/file_utils.py
+++ src/file_utils.py
@@ -1,5 +1,7 @@
 import os
+import json
 from logger import Logger
+
 
 file_utils_logger = Logger("[FileUtils]")
 
@@ -15,3 +17,27 @@ def get_file_data(filename, mode):
             
     file_utils_logger.log(f"<get_file_data>: File not found -> \"{filename}\" ")
     return None
+
+
+def get_file_without_ext(filename):
+    return os.path.splitext(filename)[0]
+
+def write_to_file(filename, data, mode):
+    file = open(filename, mode)
+    file.write(data)
+    file.close()
+    file_utils_logger.log(f"<write_to_file: written file data to {filename}")
+
+
+def write_to_file_json(filename, data, mode):
+    file = open(filename, mode)
+    data = json.dumps(data)
+    file.write(data)
+    file.close()
+    file_utils_logger.log(f"<write_to_file_json: written file data to {filename}")
+
+    
+
+
+    
+        
