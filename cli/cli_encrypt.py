55682c814ab9297eb03c0a3e52415da93a8624cf
Tamandeep Singh <74382237+Tamandeep-Singh@users.noreply.github.com>
2023-08-14 21:41:26 +0100

    Fixed capitalisation for project directory

--- cli/cli_encrypt.py
+++ cli/cli_encrypt.py
@@ -1,6 +1,6 @@
 import sys
 import argparse
-sys.path.insert(0, '/home/tam/Projects/File-Encrypt/lib')
+sys.path.insert(0, '/home/tam/projects/file-encrypt/lib')
 import file_encrypt as FileEncrypt
 
 def get_parser() -> argparse.ArgumentParser:
